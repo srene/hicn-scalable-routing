778ebdb6d85ea8b6bc8eb8e1a1c9b2731f40dc43
sergi <sergi@srenevic.net>
2021-01-21 12:21:43 +0100

    network scenario

--- hicn-docker/network/startvpp_router2.sh
+++ hicn-docker/network/startvpp_router2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 cat > /etc/vpp/config.txt << EOL
-create memif socket id 1 filename /memif/memif1.sock
 create memif socket id 2 filename /memif/memif2.sock
-create interface memif id 0 socket-id 1 master
-create interface memif id 0 socket-id 2 master
-set int state memif1/0 up
-set int ip addr memif1/0 fd00::2/64
+create memif socket id 4 filename /memif/memif4.sock
+create interface memif id 0 socket-id 2 slave
+create interface memif id 0 socket-id 4 slave
 set int state memif2/0 up
-set int ip addr memif2/0 fd01::2/64
-ip route add b002::1/64 via fd00::1 memif1/0
+set int state memif4/0 up
+set int ip addr memif2/0 fd01::1/64
+set int ip addr memif4/0 fd03::1/64
+#ip route add b002::1/64 via fd01::2 memif2/0
 EOL
 
 sleep 5
